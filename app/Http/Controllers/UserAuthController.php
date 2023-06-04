cddad53cfe381aee206cd47aa4322ff785ebfab7
AbdElOuahab Radja <abdelouahabradja@gmail.com>
2023-06-04 19:15:27 +0100

    restoring this controller which has been deleted by mistake hhh

---
+++ app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAuthController extends Controller
+{
+    public function login()
+    {
+    }
+    public function signup()
+    {
+    }
+    public function logout()
+    {
+    }
+}
