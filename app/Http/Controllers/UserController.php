44717317cdae689cbef901f2780ab95f2ddf57d5
AbdElOuahab Radja <abdelouahabradja@gmail.com>
2023-06-06 01:32:28 +0100

    added User Controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function test()
+    {
+        $userId = 4;
+        $user = User::find($userId);
+        $profile = $user->profile; // Retrieve the associated profile
+        // You can access profile attributes like this:
+        $profileName = $profile->name;
+        $profileEmail = $profile->email;
+    }
 }
