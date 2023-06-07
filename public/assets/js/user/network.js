ae1ce084671e188538d4d125ec2a6a9bf44bb0f7
AbdElOuahab Radja <abdelouahabradja@gmail.com>
2023-06-07 15:54:42 +0100

    add network view for the user to manage friend relatioships etc

--- public/assets/js/user/network.js
+++ public/assets/js/user/network.js
@@ -1,1 +1,66 @@
 console.log("welcome to network page");
+
+// Sample data for user profiles
+const users = [
+  {
+    name: "John Doe",
+    occupation: "Doctor",
+    department: "Pneumology",
+    hospital: "ABC Hospital",
+    city: "New York",
+    photo: "https://randomuser.me/api/portraits/men/1.jpg"
+  },
+  {
+    name: "Jane Smith",
+    occupation: "Nurse",
+    department: "Pneumology",
+    hospital: "XYZ Hospital",
+    city: "Los Angeles",
+    photo: "https://randomuser.me/api/portraits/women/2.jpg"
+  },
+  // Add more sample users here
+];
+
+// Function to generate user profile cards
+function generateUserProfileCard(user) {
+  return `
+          <div class="col-md-4">
+            <div class="card mb-3">
+              <img src="${user.photo}" class="card-img-top" alt="User Photo">
+              <div class="card-body">
+                <h5 class="card-title fw-bold">${user.name}</h5>
+                <p class="card-text fst-italic">
+                <i class="fa-solid fa-user-doctor"></i> ${user.occupation}
+                </p>
+                <p class="card-text fst-italic">
+                <i class="fa-solid fa-building-user"></i> ${user.department}
+                </p>
+                <p class="card-text fst-italic">
+                <i class="fa-solid fa-hospital"></i> ${user.hospital}
+                </p>
+                <p class="card-text fst-italic">
+                <i class="fa-solid fa-location-dot"></i> ${user.city}
+                </p>
+              </div>
+              <div class="card-footer">
+                <button class="btn btn-outline-custom-primary btn-add">Add</button>
+                <button class="btn btn-outline-custom-warning">Remove</button>
+              </div>
+            </div>
+          </div>
+        `;
+}
+
+// Function to display user profile cards
+function displayUserProfiles() {
+  const userProfileContainer = document.getElementById('user-profiles');
+  userProfileContainer.innerHTML = '';
+
+  for (let user of users) {
+    const card = generateUserProfileCard(user);
+    userProfileContainer.innerHTML += card;
+  }
+}
+
+// Display initial user profiles
+displayUserProfiles();
--- resources/views/user/network.blade.php
+++ resources/views/user/network.blade.php
@@ -13,8 +13,61 @@
 
 
 @section('page-content')
-    <h3>Grow your Network</h3>
-    
+    <div class="container text-custom-dark">
+        <h3 class="text-capitalize">manage your network</h3>
+        <div class="row mt-3 align-items-end">
+            <div class="col-md-12">
+                <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Search users">
+                    <button class="btn btn-outline-custom-primary" type="button">Search</button>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <div class="row mt-2">
+                    <div class="col-md-3">
+                        <label for="city-filter" class="form-label fw-bold">
+                            City:
+                        </label>
+                        <select class="form-select" id="city-filter">
+                            <option selected>All</option>
+                            <!-- Add city options here -->
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="hospital-filter" class="form-label fw-bold">
+                            Hospital:
+                        </label>
+                        <select class="form-select" id="hospital-filter">
+                            <option selected>All</option>
+                            <!-- Add hospital options here -->
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="department-filter" class="form-label fw-bold">
+                            Department:
+                        </label>
+                        <select class="form-select" id="department-filter">
+                            <option selected>All</option>
+                            <!-- Add department options here -->
+                        </select>
+                    </div>
+                    <div class="col-md-3">
+                        <label for="department-filter" class="form-label fw-bold">
+                            Occupation:
+                        </label>
+                        <select class="form-select" id="department-filter">
+                            <option selected>All</option>
+                            <!-- Add department options here -->
+                        </select>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row mt-3" id="user-profiles">
+            <!-- User profile cards will be dynamically added here -->
+        </div>
+    </div>
+
 @endsection
 
 @section('script')
